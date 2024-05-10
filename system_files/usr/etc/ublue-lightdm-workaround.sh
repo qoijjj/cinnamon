9f9a2423256c04faea350bfb72c21340b88f93bf
Joe Ipson <joe@ipson.me>
2024-05-10 15:00:07 -0600

    Update ublue-lightdm-workaround.sh

--- system_files/usr/etc/ublue-lightdm-workaround.sh
+++ system_files/usr/etc/ublue-lightdm-workaround.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d /var/cache/lightdm ]; then mkdir /var/cache/lightdm; fi
 if [ ! -d /var/log/lightdm ]; then mkdir /var/log/lightdm; fi
-if [ ! -d /var/lib/lightdm ]; then mkdir /var/lib/lightdm; fi
+if [ ! -d /var/lib/lightdm/.config/dconf ]; then mkdir -p /var/lib/lightdm/.config/dconf; fi
 if [ ! -d /var/lib/lightdm-data ]; then mkdir /var/lib/lightdm-data; fi
 
 getent group lightdm >/dev/null || groupadd -r lightdm
